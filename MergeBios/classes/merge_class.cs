74f9b85e701dba9ff2398d68ba3a5edf155f3b26
C. Herrera <pro.cah@gmail.com>
2018-03-08 17:56:52 -0600

    Log plus Merge changes
    Log helper to check the fire of proper events

--- MergeBios/classes/merge_class.cs
+++ MergeBios/classes/merge_class.cs
@@ -18,8 +18,8 @@ namespace MergeBios
         private string sbios_path;     // bios file path
         private string preboot_ver;    // preboot ver label
         private string preboot_path;   // ptreboot file path
-        private string modvbt_name     // VBT file
-        private string modvbt_path;    // VBT file path        
+        private string vbt_name;       // default VBT file
+        private string vbt_path;       // default VBT file path        
         private string merge_final_name; // name composite from selection on the ui
 
         private string custom_vbt_name; // name of the custom vbt
@@ -35,9 +35,26 @@ namespace MergeBios
         private bool use_prod_ft;
         private bool use_lfp_ft;
 
+
+        /// <summary>
+        /// Public constructro, no overloaded.
+        /// </summary>
         public Merge ()
         {
             // TBD
+            sbios_path = string.Empty;
+            sbios_ver = string.Empty;
+
+            preboot_ver = string.Empty;
+            preboot_path = string.Empty;
+
+            vbt_name = string.Empty;
+            vbt_path = string.Empty;
+
+            merge_final_name = string.Empty;
+
+            custom_vbt_name = string.Empty;
+            custom_vbt_path = string.Empty;
         }
 
         /// <summary>
@@ -46,7 +63,7 @@ namespace MergeBios
         /// <param name="mergeExe"></param>
         /// <param name="sBIOS"></param>
         /// <param name="preBoot"></param>
-        public void Create_mergeBios (string mergeExe, string sBIOS, string preBoot)
+        public void SC_Create_merged_Bios (string mergeScript, string sBIOS, string preBoot)
         {
 
             // Put instrucctions to start merge
@@ -60,11 +77,27 @@ namespace MergeBios
         /// <param name="sBIOS"></param>
         /// <param name="preBoot"></param>
         /// <param name="VBTFile"></param>
-        public void Create_mergeBios (string mergeExe, string sBIOS, string preBoot, string VBTFile)
+        public void SC_Create_merged_Bios (string mergeScript, string sBIOS, string preBoot, string VBTFile)
         {
 
             // Put instrucctions to start merge
 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void NS_Create_BIOS(int Gen, int Preboot_type )
+        {
+            // Placeholder
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void NS_Create_BIOS_nC(int Gen, int Preboot_type)
+        {
+            // Placeholder
         }
 
         /// <summary>
@@ -91,8 +124,86 @@ namespace MergeBios
 
         #region Accesors
 
+        /// <summary>
+        /// Gets or set the new path to IFWI files
+        /// </summary>
+        public string IFWI_Path
+        {
+            get { return sbios_path;  }
+            set { sbios_path = value; }
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public string IFWI_label
+        {
+            get { return sbios_ver; }
+            set { sbios_ver = value; }
+        }
 
+        /// <summary>
+        /// Gets or set the preboot path for merge
+        /// </summary>
+        public string PreBoot_Path
+        {
+            get { return preboot_path;  }
+            set { preboot_path = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PreBoot_label
+        {
+            get { return preboot_ver; }
+            set { preboot_ver = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DefaultVBT_Path
+        {
+            get { return vbt_path; }
+            set { vbt_path = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DefaultVBT_label
+        {
+            get { return vbt_name; }
+            set { vbt_name = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CustomVBT_Mod_Path
+        {
+            get { return custom_vbt_path; }
+            set { custom_vbt_path = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CustomVBT_Mod_label
+        {
+            get { return custom_vbt_name; }
+            set { custom_vbt_name = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MergeBIOS_label
+        {
+            get { return merge_final_name; }
+            set { merge_final_name = value; }
+        }
 
         #endregion
 
